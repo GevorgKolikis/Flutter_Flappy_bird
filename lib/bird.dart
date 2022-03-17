8517973f3d0299687cb85d8401c0177b8ffb65e3
GevorgKolikis <gevorg3035@gmail.com>
2022-03-17 18:19:45 +0400

    first commit

---
+++ lib/bird.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyBird extends StatelessWidget {
+  const MyBird({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'lib/img/flappybird.png'
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_flappy_bird/bird.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.blue,
+              child: MyBird(),
+            ),
+          ),
+           Expanded(
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
